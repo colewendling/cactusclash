a089ab917952ef873b9994b1ed1f00dc57b1f96b
colewendling <cole@wendling.io>
2024-01-05 13:19:24 -0600

    update homepage game window styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 'use client'; // make it a client component
 
 import { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
   const [gameStarted, setGameStarted] = useState(false);
@@ -17,17 +18,34 @@ export default function Home() {
   }, [gameStarted]);
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h1>Home</h1>
-      {!gameStarted && (
-        <button onClick={() => setGameStarted(true)}>Start Game</button>
+    <div className="flex flex-col items-center justify-center p-8">
+      {!gameStarted ? (
+        <>
+          <div className="w-full h-[720px] relative">
+            <Image
+              src="/placeholder/home.png"
+              alt="Placeholder for Home Screen"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="flex items-center justify-center w-full p-8">
+            <button
+              className="text-xl font-semibold border-4 p-4 rounded-lg hover:text-orange-500 hover:bg-orange-500/20 hover:border-orange-800"
+              onClick={() => setGameStarted(true)}
+            >
+              Play
+            </button>
+          </div>
+        </>
+      ) : (
+        <canvas
+          ref={canvasRef}
+          width={1280}
+          height={720}
+          style={{ display: gameStarted ? 'inline-block' : 'none' }}
+        />
       )}
-      <canvas
-        ref={canvasRef}
-        width={1280}
-        height={720}
-        style={{ display: gameStarted ? 'inline-block' : 'none' }}
-      />
     </div>
   );
 }
