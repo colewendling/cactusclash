f3cb2a7a52b8e0bf21c2e6445a6919ec0bb54dbe
Cole Wendling <cole@wendling.io>
2025-02-13 10:31:34 +0100

    add fontawesome to layout.tsx and disable auto CSS injection

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
