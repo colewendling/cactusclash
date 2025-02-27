968afd53a3b6ce81355bd7e923414d3bdbe386dc
Cole Wendling <cole@wendling.io>
2025-02-27 10:10:17 +0100

    update site to use new fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,21 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        primary: '#E46C4E',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#E46C4E",
+      },
+      fontFamily: {
+        favoritMono: ["ABCFavoritMono", "monospace"],
+        optician: ["Optician Sans", "sans-serif"],
+        robotoMono: ["Roboto Mono Variable", "monospace"],
       },
     },
   },
